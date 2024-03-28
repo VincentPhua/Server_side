f264d08bb96be578e3e31dd13129e9aaaf4b059f
Han Chung <105803853+SevenLims@users.noreply.github.com>
2024-03-28 17:00:55 +0800

    Update gadgetcart.sql to assignment_db.sql for connecting to database to reetrieve products

--- functions.php
+++ functions.php
@@ -5,7 +5,7 @@ function pdo_connect_mysql()
     $DATABASE_HOST = 'localhost';
     $DATABASE_USER = 'root';
     $DATABASE_PASS = '';
-    $DATABASE_NAME = 'gadgetcart';
+    $DATABASE_NAME = 'assignment_db';
     try 
     {
     	return new PDO('mysql:host=' . $DATABASE_HOST . ';dbname=' . $DATABASE_NAME . ';charset=utf8', $DATABASE_USER, $DATABASE_PASS);
