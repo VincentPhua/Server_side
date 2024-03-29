f3dcfa83574a41f76ecb61e9be96041674ba3658
VincentPhua <91897097+VincentPhua@users.noreply.github.com>
2024-03-29 21:32:15 +0800

    Added Session timeout

--- auth.php
+++ auth.php
@@ -1,9 +1,18 @@
 <?php
 // Start new session
 session_start();
+$timeout = 300; // Set timeout minutes
 
 // If username is not set will redirect to login page
-if(!isset($_SESSION["staff_name"])){
-header("Location: login.php");
-exit(); }
+if(!isset($_SESSION["staff_name"]) || (time() - $_SESSION['last_timestamp']) >= $timeout) {
+    echo $_SESSION['last_timestamp'];
+    echo $timeout;
+    session_unset();
+    session_destroy();
+    header("Location: login.php?session_expired=1");
+    exit(); 
+}else{
+    session_regenerate_id(true);
+    $_SESSION['last_timestamp'] = time();
+}
 ?>
